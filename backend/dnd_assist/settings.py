150a39b86f8533df966499eda59cfb2176ec3f79
Dmitry Shakhovsky <noiqtas.nsk@gmail.com>
2024-10-12 21:16:30 +0300

    Add and update dockerfile and docker-compose

--- backend/dnd_assist/settings.py
+++ backend/dnd_assist/settings.py
@@ -79,14 +79,15 @@ WSGI_APPLICATION = 'dnd_assist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Используется PostgreSQL
-        'NAME': 'postgres',  # Имя базы данных
-        'USER': 'postgres',  # Имя пользователя
-        'PASSWORD': 'postgres',  # Пароль пользователя
-        'HOST': 'pgdb',  # Наименование контейнера для базы данных в Docker Compose
-        'PORT': '5432',  # Порт базы данных
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dnd-assist',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
