15eba2983aff02c1d4fab5ce8dadb0f821cd1331
DemkaCblch <krotov.dimon2013@yandex.ru>
2024-10-13 19:29:19 +0300

    Add and update dockerfile and docker-compose

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return HttpResponse("login")
+
+def pageNotFound(request, exception):
+    return  HttpResponseNotFound('<img src="https://avatars.mds.yandex.net/i?id=6327d4e69260d1c93797d2b806b31aa1_sr-9242319-images-thumbs&n=13"/>')
