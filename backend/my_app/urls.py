fbe0e58785ea06a444aa9a35459d9f4baa0a46a3
DemkaWork <krotov.dimon2013@yandex.ru>
2024-10-18 12:34:47 +0300

    Подключил Redis

--- backend/my_app/urls.py
+++ backend/my_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-
 from .views import *
+from .consumers import GameConsumer
+
+websocket_urlpatterns = [
+    path('ws/game/<str:room_name>/', GameConsumer.as_asgi()),  # Путь для WebSocket
+]
 
 urlpatterns = [
     path('api/login/', LoginView.as_view(), name='login'),
