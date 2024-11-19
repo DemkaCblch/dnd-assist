53dca075a2203faf8b1780976b3570c15308e134
DemkaCblch <krotov.dimon2013@yandex.ru>
2024-11-19 23:15:25 +0300

    Some fixes

--- backend/dnd_assist/celery.py
+++ backend/dnd_assist/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dnd-assist.settings')
-app = Celery('dnd-assist')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dnd_assist.settings')
+app = Celery('dnd_assist')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
