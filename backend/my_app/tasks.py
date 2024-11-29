f59f4355f40b6414e6aec579dc35fb1a824619db
Dmitry Shakhovsky <noiqtas.nsk@gmail.com>
2024-11-29 18:20:30 +0300

    register task update

--- backend/my_app/tasks.py
+++ backend/my_app/tasks.py
@@ -3,5 +3,10 @@ from django.core.mail import send_mail
 # from .models import .....
 
 @shared_task
-def account_created():
-    pass
+def account_created(account_id):
+    account = ''
+    subject = ''
+    message =  'you have successfully registered!'
+    mail_sent = send_mail(subject, message, 'admin@dnd-assist.com', [])
+
+    return mail_sent
