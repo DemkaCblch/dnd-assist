42985d05d23d5b1bac406a88e72ad90f78109817
Dmitry <143360140+SHVDmitry@users.noreply.github.com>
2024-12-20 02:49:57 +0300

    Update entrypoint.sh
    pool=solo

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,5 +2,5 @@
 python manage.py makemigrations room
 python manage.py makemigrations user_profile
 python manage.py migrate
-celery -A dnd_assist worker --loglevel=info &
+celery -A dnd_assist worker --loglevel=info --pool=solo &
 python manage.py runserver 0.0.0.0:8000
